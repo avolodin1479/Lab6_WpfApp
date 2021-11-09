1c8aebf1c49c4dcf4e8aff954ebe7dbf80857b96
avolodin1479@gmail.com <avolodin1479@gmail.com>
2021-11-09 12:35:48 +0600

    Добавьте файлы проекта.

---
+++ Lab6_WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab6_WpfApp
+{
+    class WeatherControl : DependencyObject
+    {
+        private string wind;
+        private int speedWind;
+        private enum Precipitation
+        {
+            Sunny,
+            Cloudy,
+            Rain,
+            Snow
+        }
+        public static readonly DependencyProperty TemperatureProperty = DependencyProperty.Register(
+            nameof(Temperature),
+            typeof(int),
+            typeof(WeatherControl),
+            new FrameworkPropertyMetadata(
+                0,
+                FrameworkPropertyMetadataOptions.AffectsMeasure |
+                FrameworkPropertyMetadataOptions.AffectsRender,
+                null,
+                new CoerceValueCallback(CoerceTemperature))
+                        );
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int t = (int)baseValue;
+            if (t >= -50 && t <= 50)
+                return t;
+            else
+                return 0;
+        }
+
+        public string Wind { get; set; }
+        public int SpeedWind;
+
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+    }
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
